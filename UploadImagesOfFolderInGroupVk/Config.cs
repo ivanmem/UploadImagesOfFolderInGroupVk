a9fd17ec7caf90069df8fc367064374b1de1f922
Ivan Melentyev <ivan.saxalin@mail.ru>
2021-02-11 22:23:32 +0300

    init

---
+++ UploadImagesOfFolderInGroupVk/Config.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace UploadImagesOfFolderInGroupVk
+{
+    [Serializable]
+    public class Config
+    {
+        public string[] AccessTokens = Array.Empty<string>();
+        public long GroupId;
+        public long AlbumId;
+        public long Skip = 0;
+        public string? PathFiles;
+
+        public static Config Get()
+        {
+            string settingsPath = Path.Combine(Directory.GetCurrentDirectory(), "settings.json");
+
+            static void Error()
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Заполните конфигурацию.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            
+            if (!File.Exists(settingsPath))
+            {
+                var newConfig = new Config
+                {
+                    Skip = 0,
+                    AccessTokens = new [] {"123"},
+                    AlbumId = 0,
+                    GroupId = 0,
+                    PathFiles = Directory.GetCurrentDirectory()
+                };
+                File.WriteAllText(settingsPath, JsonConvert.SerializeObject(newConfig));
+                Error();
+            }
+
+            try
+            {
+                var config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(settingsPath));
+                if (config.AlbumId == 0 || config.GroupId == 0 || config.AccessTokens.Length == 0)
+                {
+                    Error();
+                }
+
+                config.PathFiles ??= Directory.GetCurrentDirectory();
+                return config;
+            }
+            catch
+            {
+                Error();
+            }
+            
+            // Сюда не дойдёт, ибо в случае ошибки консолька выключается
+            return new ();
+        }
+    }
+}
---
+++ UploadImagesOfFolderInGroupVk/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Exception;
+
+namespace UploadImagesOfFolderInGroupVk
+{
+    class Program
+    {
+        public static IVkApi Api
+        {
+            get
+            {
+                _prevApiIndex++;
+                if (_prevApiIndex >= Apis.Length) _prevApiIndex = 0;
+                return Apis[_prevApiIndex];
+            }
+        }
+
+        private static int _prevApiIndex = -1;
+        public static IVkApi[] Apis = Array.Empty<IVkApi>();
+
+        static void Main()
+        {
+            var conf = Config.Get();
+            Console.WriteLine($@"
+AccessTokens Length: {conf.AccessTokens.Length}
+PathFiles: {conf.PathFiles}
+GroupId: {conf.GroupId}
+AlbumId: {conf.AlbumId}
+Skip: {conf.Skip}"
+            );
+            Apis = conf.AccessTokens.Select(x =>
+            {
+                var api = new VkApi();
+                api.Authorize(new ()
+                {
+                    AccessToken = x.Trim()
+                });
+                return api;
+            }).ToArray();
+            var files = Directory.GetFiles(conf.PathFiles!);
+            Console.WriteLine($"Найдено {files.Length} файлов.");
+            var wc = new WebClient();
+            var newLine = true;
+            for (var i = conf.Skip; i < files.Length; i++)
+            {
+                if (Apis.Length == 0)
+                {
+                    Console.WriteLine($"Токены без лимита закончились. Всего выполнилось: {i-1}.");
+                    break;
+                }
+                
+                var file = files[i];
+                var api = Api;
+                try
+                {
+                    var uploadServer = api.Photo.GetUploadServer(conf.AlbumId, conf.GroupId);
+                    var responseImg = Encoding.ASCII.GetString(wc.UploadFile(uploadServer.UploadUrl, file));
+                    api.Photo.Save(new()
+                    {
+                        GroupId = conf.GroupId,
+                        AlbumId = conf.AlbumId,
+                        SaveFileResponse = responseImg
+                    });
+                }
+                catch (Exception ex)
+                {
+                    newLine = true;
+                    Console.WriteLine();
+                    if (ex is ParameterMissingOrInvalidException)
+                    {
+                        Console.WriteLine($"Ошибка загрузки {file}. Текущий индекc: {i}. Пропущено.");
+                        continue;
+                    }
+                    
+                    i--;
+                    if (ex is TooManyRequestsException)
+                    {
+                        Apis = Apis.Where(x => x.Token != api.Token).ToArray();
+                        Console.WriteLine($"Токен {new string(api.Token.Take(5).ToArray())} удалён из-за TooManyRequestsException. Текущий индекc: {i}.");
+                        continue;
+                    }
+
+                    if (ex is TooMuchOfTheSameTypeOfActionException)
+                    {
+                        Apis = Apis.Where(x => x.Token != api.Token).ToArray();
+                        Console.WriteLine($"Токен {new string(api.Token.Take(5).ToArray())} удалён из-за TooMuchOfTheSameTypeOfActionException");
+                        continue;
+                    }
+                    Console.WriteLine($"Ошибка загрузки {file}. Текущий индекc: {i}.");
+                    continue;
+                }
+                
+                var str = $"На текущий момент загружено {i} фотографий.";
+                if (newLine) Console.WriteLine();
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(str);
+                newLine = false;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Загрузка завершена.");
+            Console.ReadLine();
+        }
+    }
+}
